bf8dc8cbd75f7b82d422fddab6e10ab65b93e35f
VarishTyagi <varishtyagi351@gmail.com>
2024-07-10 19:37:19 +0530

    Error resolve

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -16,9 +16,8 @@ import { IImage } from "@/lib/database/models/image.model";
 import { formUrlQuery } from "@/lib/utils";
 
 import { Button } from "../ui/button";
+
 import { Search } from "./Search";
-
-
 
 export const Collection = ({
   hasSearch = false,
